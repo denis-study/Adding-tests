beb65aaed9b9d95d70147b4749a6c74649bc1dc0
Denis T <dengertd@gmail.com>
2021-02-06 17:35:20 +0300

    modify

--- src/main/java/BonusMilesService.java
+++
@@ -1,7 +0,0 @@
-public class BonusMilesService {
-    public  int calculate(int cost) {
-        int count = 20;
-        int miles = (cost / count);
-        return miles;
-       }
-}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,25 @@
+
 public class Main {
     public static void main(String[] args) {
-        BonusMilesService service = new BonusMilesService();
-        int cost = 10_000;
-        int expected = 500;
+        BonusService service = new BonusService();
 
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
 
-        int actual = service.calculate(cost);
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
 
+        // производим проверку (сравниваем ожидаемый и фактический):
+        // если true - то PASS
+        // если false - то FAIL
         boolean passed = expected == actual;
 
+        // выводим результат
         System.out.println(passed);
     }
 }
-
 
 
 
